49c82c58488b94672d1ecd921618bcb40f69956d
David <David.NicolasGarcia@student.csulb.edu>
2024-12-13 21:34:56 -0800

    moved dataset to folder

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,28 @@
-# Loads the dataset to be used by both models
+class Animals_loader:
+    """
+    Data Loader for the Animal-10 dataset. Contains useful functions that the class can perform on the dataset:
+    Loading the dataset/prepping it, datasplitting.
+    """
+
+    x_train, y_train, x_sub_train, y_subtrain, x_val, y_val, x_test, y_test = (
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+    )
+
+    def __init__(self):
+
+        pass
+
+    # Split the data into training, and test set
+    def split(self):
+        pass
+
+    # Split the data into training: {x_sub_train,y_sub_train, x_validation,y_validation} and test set
+    def split_validation(self):
+        pass
