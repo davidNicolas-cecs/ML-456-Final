18e35a7ddb690913b2f6488ccc99841a7fccb546
David <David.NicolasGarcia@student.csulb.edu>
2024-12-17 16:40:23 -0800

    finished setup now testing model 1

---
+++ model1/model1.py
@@ -0,0 +1,57 @@
+# from data_loader import Animals_loader
+# from ..utils import encode, hot_encode
+# from keras.models import Sequential
+# from keras.layers import (  # type: ignore
+#     Dense,
+#     Activation,
+#     InputLayer,
+#     Flatten,
+#     Conv2D,
+#     MaxPooling2D,
+#     Dropout,
+#     BatchNormalization,
+# )
+# from keras.optimizers import RMSprop
+
+
+# def prep():
+#     # Prepping the data
+#     animal_loader = Animals_loader()
+#     img_dataset = animal_loader.create_dataset()
+#     x, y = animal_loader.image_to_array(img_dataset)
+#     y = encode(y)
+#     y = hot_encode(y)
+#     print(y)
+#     return animal_loader.split(x, y)
+
+
+# def build_model(hp):
+#     model = Sequential()
+#     model.add(
+#         Conv2D(
+#             filters=hp.Int(f"filters_{0}", min_value=32, max_value=512, step=32),
+#             kernel_size=(3, 3),
+#             activation="relu",
+#             padding=hp.Choice("padding", ["same", "valid"]),
+#             strides=(1, 1),
+#             input_shape=(32, 32, 3),
+#         )
+#     )
+#     for i in range(hp.Int("num_layers_conv", 3, 7)):
+#         pass
+
+
+# def main():
+#     (
+#         x_train,
+#         x_test,
+#         y_train,
+#         y_test,
+#     ) = prep()
+
+
+# if __name__ == "__main__":
+#     print(
+#         "Starting Model 1 dataset\n: * Contains 10 layers, 8 Conv2D, and 2 Dense Layers"
+#     )
+#     main()
